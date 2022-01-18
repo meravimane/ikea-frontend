e995fecea1e8736a3b8ee1587c99dbfbbb7dc378
meravimane <meravimane@gmail.com>
2022-01-18 20:14:26 +0530

    Products page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Product } from './Components/Products';
 
 function App() {
   return (
     <div className="App">
       <h1>Ikea Project</h1>
+
+      <Product/>
     </div>
   );
 }
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,1 +1,187 @@
-//here you can start
+
+import '../Css/Products.css'
+
+export const Product = () => {
+    return (
+        <div id="mainProductsDiv">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+            <div id="productDescription">
+                <h1>Desks for home</h1>
+                <h2>A Place For Your Computer, Stationery, And Coffee Mug Too</h2>
+                <div>
+                    <p>Whether you are creating a home office or looking for a place to keep your computers, we’ve designed our desks for home to fulfill your every need. You can choose the BEKANT sit or stand desk if you work long hours from home or the traditional LOMMARP if you like to give your space a personalised look. They even come with built-in cable management! Want storage options? There’s the KALLAX shelf-desk combo that’s perfect for saving you room and the solid wood HEMNES that comes with drawers and compartments to store your files and stationery.</p>
+               </div>
+               
+            </div>
+            <div>
+                <div id="productFilter">
+                    <button>Sort  <i class="fas fa-angle-down fa-lg"></i></button>
+                    <button>Size  <i class="fas fa-angle-down fa-lg"></i></button>
+                    <button>Price  <i class="fas fa-angle-down fa-lg"></i></button>
+                    <button>Color  <i class="fas fa-angle-down fa-lg"></i></button>
+                    <button>Material  <i class="fas fa-angle-down fa-lg"></i></button>
+                    <button>Features  <i class="fas fa-angle-down fa-lg"></i> </button>
+                    <button>All filters  <i class="fas fa-filter"></i> </button>
+                </div>
+                <div>
+                    
+
+                </div>
+            </div>
+            
+            <br />
+            <hr />
+
+            <div id="allProducts">
+                
+            
+                <div id="products">
+                    <img className="mainImg" src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=s" alt="" />
+
+                    <h4>MICKE</h4>
+
+                    <p>Desk105x50 cm (41 3/8x19 5/8 ")</p>
+
+                    <div className="price"> 
+                        <p>Rs.</p> <h3>7,690</h3>
+                    </div>
+
+                    <div className='productRating'>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>  <p>(45)</p>
+                    </div>
+                    <p>More varients</p>
+
+                    <div className="moreVarient">
+                        <img src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-anthracite-red__0921882_pe787985_s5.jpg?f=xu" alt="" />
+                    </div>
+                </div>
+
+                <div id="products">
+                    <img className="mainImg" src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=s" alt="" />
+
+                    <h4>MICKE</h4>
+
+                    <p>Desk105x50 cm (41 3/8x19 5/8 ")</p>
+
+                    <div className="price"> 
+                        <p>Rs.</p> <h3>7,690</h3>
+                    </div>
+
+                    <div className='productRating'>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>  <p>(45)</p>
+                    </div>
+                    <p>More varients</p>
+
+                    <div className="moreVarient">
+                        <img src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-anthracite-red__0921882_pe787985_s5.jpg?f=xu" alt="" />
+                    </div>
+                </div>
+
+                <div id="products">
+                    <img className="mainImg" src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=s" alt="" />
+
+                    <h4>MICKE</h4>
+
+                    <p>Desk105x50 cm (41 3/8x19 5/8 ")</p>
+
+                    <div className="price"> 
+                        <p>Rs.</p> <h3>7,690</h3>
+                    </div>
+
+                    <div className='productRating'>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>  <p>(45)</p>
+                    </div>
+                    <p>More varients</p>
+
+                    <div className="moreVarient">
+                        <img src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-anthracite-red__0921882_pe787985_s5.jpg?f=xu" alt="" />
+                    </div>
+                </div>
+
+                <div id="products">
+                    <img className="mainImg" src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=s" alt="" />
+
+                    <h4>MICKE</h4>
+
+                    <p>Desk105x50 cm (41 3/8x19 5/8 ")</p>
+
+                    <div className="price"> 
+                        <p>Rs.</p> <h3>7,690</h3>
+                    </div>
+
+                    <div className='productRating'>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>  <p>(45)</p>
+                    </div>
+                    <p>More varients</p>
+
+                    <div className="moreVarient">
+                        <img src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-anthracite-red__0921882_pe787985_s5.jpg?f=xu" alt="" />
+                    </div>
+                </div>
+
+                <div id="products">
+                    <img className="mainImg" src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=s" alt="" />
+
+                    <h4>MICKE</h4>
+
+                    <p>Desk105x50 cm (41 3/8x19 5/8 ")</p>
+
+                    <div className="price"> 
+                        <p>Rs.</p> <h3>7,690</h3>
+                    </div>
+
+                    <div className='productRating'>
+                        <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>  <p>(45)</p>
+                    </div>
+                    <p>More varients</p>
+
+                    <div className="moreVarient">
+                        <img src="https://www.ikea.com/in/en/images/products/micke-desk-white__0736018_pe740345_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-black-brown__0735981_pe740299_s5.jpg?f=xu" alt="" /> <img src="https://www.ikea.com/in/en/images/products/micke-desk-anthracite-red__0921882_pe787985_s5.jpg?f=xu" alt="" />
+                    </div>
+                </div>
+
+            </div>
+            
+
+            <hr />
+
+            <div id="relatedProduct">
+                <h3>Related categories</h3>
+                <div className='relatedProductImages'>
+                     <div id="relatedProductImg">
+                        <img src="https://www.ikea.com/global/assets/navigation/images/table-desk-systems-47423.jpeg?imwidth=400" alt="" /> <p>Table & desk systems</p>
+                    </div>
+                    <div id="relatedProductImg">
+                        <img src="https://www.ikea.com/global/assets/navigation/images/standing-desks-55008.jpeg?imwidth=400" alt="" /> <p>standing desks</p>
+                    </div>
+                    <div id="relatedProductImg">
+                        <img src="https://www.ikea.com/global/assets/navigation/images/table-desk-systems-47423.jpeg?imwidth=400" alt="" /> <p>Table & desk systems</p>
+                    </div>
+                    <div id="relatedProductImg">
+                        <img src="https://www.ikea.com/global/assets/navigation/images/standing-desks-55008.jpeg?imwidth=400" alt="" /> <p>standing desks</p>
+                    </div>
+
+                    <div id="relatedProductImg">
+                        <img src="https://www.ikea.com/global/assets/navigation/images/table-desk-systems-47423.jpeg?imwidth=400" alt="" /> <p>Table & desk systems</p>
+                    </div>
+                    
+                    
+                </div>
+               
+            </div>
+
+            <div id="bottomProductDescription">
+                
+                <h2>Work, Study Or Storage - We’ve Got A Desk For It All!</h2>
+                <div>
+                    <p>Look no further than our collection of desks for home if you want a space to work, study, or indulge in your hobby. You can choose our flexible BEKANT if you put in long hours from home. The electric switch on the underframe lets you adjust its height - set it to sit or standing positions, whichever makes you feel better. We’ve designed it with a contoured tabletop, so your arms have a place to rest. Our blue-green LOMMARP desk is the perfect example of traditional design and adds a personal touch to your bedroom or study area. Like the BEKANT, it also comes with a built-in cable management system to tuck those pesky wires away. There’s a pull out drawer to store your stationery too! Need more storage? Get the KALLAX combo to keep your speakers and consoles. And our solid-wood HEMNES looks elegant and comes with drawers and shelves. The top drawer is fitted with a small compartment for your pens and paper clips while the bottom one has a file frame to store your A4, legal and other documents.</p>
+               </div>
+               
+            </div>
+
+
+           {/* <img src="https://img.icons8.com/ios/14/000000/expand-arrow--v1.png" /> */}
+        </div>
+    )
+}
