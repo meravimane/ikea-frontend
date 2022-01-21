ba51ca3e86c93c444b8ee7c1920584e0371cad3d
ipabitrasaikia1 <79037063+ipabitrasaikia1@users.noreply.github.com>
2022-01-21 12:09:03 +0530

    Updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,7 +14,7 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
 
 
-    return <div id='navBar'>
+    return <div>  <div id='navBar'>
 
         <div id='hamburgerLeft'>
             <HBRight></HBRight>
@@ -24,7 +24,7 @@ export const Navbar = () => {
             <img src='https://www.ikea.com/in/en/static/ikea-logo.f7d9229f806b59ec64cb.svg' id="ikeaLogo" alt="" />
 
 
-            <nav>
+            <nav id='navLi'>
                 <ul>
                     <li>Products</li>
                     <li id="rooms">Rooms</li>
@@ -35,7 +35,7 @@ export const Navbar = () => {
 
 
             <div className="search-container">
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg>
+                <svg id='searchLogo' stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg>
                 <input id="myInput" onClick={() => { }} placeholder="What are you looking for?" type="text" className="search" />
                 <button id="searchbtn" type="button" onClick={() => { }}>
 
@@ -61,5 +61,7 @@ export const Navbar = () => {
 
         </div>
 
+    </div>
+        <hr id='bar' />
     </div>
 }
